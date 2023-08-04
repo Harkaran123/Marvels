a4ffc5663c019c3a892d798f567f29e7c02a3809
HappyGames <harkaransingh528@gmail.com>
2023-08-04 21:27:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const menuBox = document.querySelector(".navbar-links");
+
+function showMenu(){
+
+    menuBox.style.left = "0%";
+}
+
+function hideMenu(){
+
+    menuBox.style.left = "-100%"
+}
